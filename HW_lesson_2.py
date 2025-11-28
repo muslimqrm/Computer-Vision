bb9d2a9a6f502ef73a6701b29be5bc814d6923fd
muslimqrm <mscczn@gmail.com>
2025-11-28 09:57:11 +0200

    Update HW_lesson_2.py

--- HW_lesson_2.py
+++ HW_lesson_2.py
@@ -1,5 +1,5 @@
 import cv2
-#
+
 # image = cv2.imread('images/selfie.jpg')
 # image = cv2.resize(image, (image.shape[1] // 4,image.shape[0] // 4))
 # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -22,4 +22,5 @@ cv2.imwrite('new_email.jpg', edges)
 
 cv2.imshow('Image', edges)
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
