3549b43a9dd09da50130d9508828dafff86fda24
muslimqrm <mscczn@gmail.com>
2025-11-29 17:01:37 +0200

    Update HW_lesson_2.py

--- HW_lesson_2.py
+++ HW_lesson_2.py
@@ -4,7 +4,7 @@ import cv2
 # image = cv2.resize(image, (image.shape[1] // 4,image.shape[0] // 4))
 # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # edges = cv2.Canny(gray, 100, 100)
-# cv2.imwrite('new_selfie.jpg', edges)
+# cv2.imwrite(Output/'new_selfie.jpg', edges)
 #
 # cv2.imshow('Image', edges)
 # cv2.waitKey(0)
@@ -18,9 +18,10 @@ gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
 edges = cv2.Canny(gray, 80, 160)
 
-cv2.imwrite('new_email.jpg', edges)
+cv2.imwrite(Output/'new_email.jpg', edges)
 
 cv2.imshow('Image', edges)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
+
